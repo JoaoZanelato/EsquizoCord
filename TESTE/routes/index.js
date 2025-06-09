5c078ba66d1edf7a8dddffcb063e347d13f89ffa
leonardotomasini <leonardotomasini20@email.com>
2025-06-09 09:21:16 -0300

    enviando

--- TESTE/routes/index.js
+++ TESTE/routes/index.js
@@ -5,14 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('Home', { title: 'Express' });
 });
+
+/* GET Login page. */
 router.get('/login', function(req, res, next) {
   res.render('Login');
 });
 
+
+/* GET Cadastro page. */
 router.get('/cadastro', function(req, res, next) {
   res.render('Cadastro');
 });
 
+/* GET Dashboard page. */
 router.get('/dashboard', function(req, res, next) {
   res.render('Dashboard');
 });
