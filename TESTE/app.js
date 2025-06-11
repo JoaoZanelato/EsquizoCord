737b9ca0686e2109b0fc12699fb91992a8402f20
João Zanelato <jgzanelato@gmail.com>
2025-06-11 15:53:03 -0300

    correção app.js bugando login

--- TESTE/app.js
+++ TESTE/app.js
@@ -5,10 +5,13 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
 
+// 1. Importa a conexão com a base de dados
+const pool = require('./db'); 
+
 // Importação das rotas
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const groupsRouter = require('./routes/groups'); // <-- Garanta que esta linha existe
+const groupsRouter = require('./routes/groups');
 
 const app = express();
 
@@ -25,25 +28,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Configuração da Sessão
 app.use(session({
-  secret: 'uma_frase_bem_secreta_para_o_esquizocord', // Mude isto para uma frase aleatória
+  secret: 'a_frase_mais_secreta_do_esquizocord', // Mude isto para uma frase aleatória
   resave: false,
   saveUninitialized: true,
   cookie: { secure: process.env.NODE_ENV === 'production' }
 }));
 
+// 2. Middleware para disponibilizar a conexão da DB para todas as rotas
+//    Esta era a parte que estava em falta.
+app.use((req, res, next) => {
+  req.db = pool;
+  next();
+});
 
 // Middleware para disponibilizar dados da sessão para as views
 app.use((req, res, next) => {
-  res.locals.user = req.session.user; 
+  res.locals.user = req.session.user;
   next();
 });
 
 // Definição das Rotas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/groups', groupsRouter); // <-- E que esta linha também existe
+app.use('/groups', groupsRouter);
 
-// catch 404 e error handler
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
